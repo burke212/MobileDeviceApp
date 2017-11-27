79cfc4b957c8097900c5b7bb6180da77e0593d8d
Hannah Onorato <hannah@Hannahs-MacBook-Pro.local>
2017-11-27 16:11:56 -0500

    Misc

--- MedicalApp/app/src/main/java/com/example/burke/medicalapp/MainActivity.java
+++ MedicalApp/app/src/main/java/com/example/burke/medicalapp/MainActivity.java
@@ -34,49 +34,49 @@ public class MainActivity extends AppCompatActivity {
 
         //testing layout of add.xml. Uncomment line beneath to load activity main first
         //setContentView(R.layout.activity_main);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        setCurrentTime();
-        setCurrentDay();
-        System.out.print("TEST");
+//        setCurrentTime();
+//        setCurrentDay();
+//        System.out.print("TEST");
 
-//        view = (Button)findViewById( R.id.view );
-//        add = (Button)findViewById( R.id.add );
-//
-//view.setOnClickListener(new View.OnClickListener() {
-//    @Override
-//    public void onClick(View v) {
-//        Intent intent = new Intent(MainActivity.this, ViewMed.class);
-//        startActivity(intent);
-//    }
-//});
-//
-//add.setOnClickListener(new View.OnClickListener() {
-//    @Override
-//    public void onClick(View v) {
-//        Intent intent = new Intent(MainActivity.this, AddActivity.class);
-//        startActivity(intent);
-//    }
-//});
-//        //set onClickListener on Time TV
-//
-//
+        view = (Button)findViewById( R.id.view );
+        add = (Button)findViewById( R.id.add );
+
+view.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, ViewMed.class);
+        startActivity(intent);
+    }
+});
+
+add.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, AddActivity.class);
+        startActivity(intent);
+    }
+});
+        //set onClickListener on Time TV
+
+
 }
 
-    private void setCurrentDay(){
-        String currentDay;
-        SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.US);
-        Calendar calendar = Calendar.getInstance();
-        currentDay = dayFormat.format(calendar.getTime());
-        System.out.println("current day: " + currentDay + "\n");
-    }
-
-    private void setCurrentTime(){
-        String currentTime;
-        Format formatter = new SimpleDateFormat("hh:mm a");
-        Calendar calendar = Calendar.getInstance();
-        currentTime = formatter.format(calendar.getTime());
-        System.out.println("current time: " + currentTime + "\n");
-    }
+//    private void setCurrentDay(){
+//        String currentDay;
+//        SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.US);
+//        Calendar calendar = Calendar.getInstance();
+//        currentDay = dayFormat.format(calendar.getTime());
+//        System.out.println("current day: " + currentDay + "\n");
+//    }
+//
+//    private void setCurrentTime(){
+//        String currentTime;
+//        Format formatter = new SimpleDateFormat("hh:mm a");
+//        Calendar calendar = Calendar.getInstance();
+//        currentTime = formatter.format(calendar.getTime());
+//        System.out.println("current time: " + currentTime + "\n");
+//    }
 
 }
